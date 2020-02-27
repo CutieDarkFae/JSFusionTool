d27de55749954c2c8a15a255f1ff7ddec11aaa51
Cutie DarkFae <erczabet@protonmail.com>
2020-02-27 18:01:44 +1100

    Got a variation-less demon list :)

--- src/loader.js
+++ src/loader.js
@@ -1,47 +1,14 @@
 const fs = require('fs')
 
-const JSON_CACHE = 'p:/work/javascript/scraper/cache/demons.json'
+const JSON_CACHE = './src/demons.json'
 
 exports.load = (() => {
   let demons = JSON.parse(fs.readFileSync(JSON_CACHE, 'utf8'))
 
-  // work out the variations
-  let parsedDemons = {...demons}
-
-  while(Object.keys(parsedDemons).length > 0) {
-    // iterate through, if we find we're a variant, remove us
-  }
-  for (let j = 0; j < Object.keys(demons).length; j++) {
-    let demon = demons[Object.keys(demons)[j]]
-    let base = true
-    for (let i = 0 ;i < Object.keys(demons).length; i++) {
-      let demon2 = demons[Object.keys(demons)[i]]
-      if (
-        demon.name.includes(demon2.name) &&
-        demon2.name.length < demon.name.length
-      ) {
-        // we've found a shorter name version, so we're a variant
-        base = false
-        break
-      }
-    }
-    // let variations = demon.variations
-    // let base = true
-    // for (let i = 0; i < variations.length; i++) {
-    //   if (variations[i].length < demon.name.length) {
-    //     // we are a variation, the demon at i is the real demon
-    //     base = false
-    //     break;
-    //   }
-    // }
-    if (base) {
-      parsedDemons[demon.name] = demon // this isn't fool proof :(
-    }
-  }
   let familyMap = {}
 
-  Object.keys(parsedDemons).forEach((key) => {
-    let demon = parsedDemons[key]
+  Object.keys(demons).forEach((key) => {
+    let demon = demons[key]
     let race = demon.stats.Race
     if (demon.familyCombinations !== undefined &&
       demon.familyCombinations.length !== 0 &&
@@ -69,7 +36,7 @@ exports.load = (() => {
   })
 
   return {
-    demons: parsedDemons,
+    demons: demons,
     familyMap: familyMap
   }
 })
